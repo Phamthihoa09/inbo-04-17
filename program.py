8a13742d28c4ac39f883fa27479b5942ba4e98d4
Dmitrii Shesterikov <shistick9906@gmail.com>
2020-10-24 17:21:23 +0300

    Update

--- program.py
+++ program.py
@@ -9,6 +9,6 @@ def hello_world():
 def hello_world_v2():
     return 'Another text'
 
-@app.route('/capchik')
+@app.route('/shistick98')
 def capchik():
-    return 'CAPCHIK task'
+    return 'I am Dmitrii Shesterikov inbo-04-17cd'
