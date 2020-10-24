49bcdd2d7c7cde992d8492aeca1c8077cb5980f4
Victor <victor.nguen@gmail.com>
2020-10-24 16:54:20 +0300

    added my route and aligned lines

--- program.py
+++ program.py
@@ -1,6 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
@@ -8,3 +9,7 @@ def hello_world():
 @app.route('/v2')
 def hello_world_v2():
     return 'Another text'
+
+@app.route('/de4d10ck')
+def about():
+    return 'Welcome to my page'
